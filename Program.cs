d236c01165a2a56625c12661adc0c393e12f8d91
MaO <company.r@Mail.ru>
2025-02-01 11:15:11 -0800

    kursi4

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Mate Shengelia");
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+var inpuy = Console.ReadLine().ToString();
+Console.WriteLine(inpuy);
+
